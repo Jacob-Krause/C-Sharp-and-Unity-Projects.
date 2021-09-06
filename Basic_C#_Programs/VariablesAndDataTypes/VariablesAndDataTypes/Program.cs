f3677cc80ce8b8ec46c6f21be5b714dc25f722a2
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-09-06 16:35:21 -0700

    Update

---
+++ Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write("What is your name:  ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        string name = Console.ReadLine();
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("What course are you on:  ");
+        string course = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("What page number:    ");
+        short pagenumber = Convert.ToInt16 (Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Do you need help with anything? (Please answer true or false.):    ");
+        bool help = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("How many hours did you study today:    ");
+        float hour = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day! This is the end of the program.");
+        Console.Read();
+        }
+    }   
+
---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {   //This Program spits a name back at you
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("What is your name:  ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello," + name + " it is very nice to meet you my name is Computer!");
+            Console.Read();
+        }
+    }
+}
