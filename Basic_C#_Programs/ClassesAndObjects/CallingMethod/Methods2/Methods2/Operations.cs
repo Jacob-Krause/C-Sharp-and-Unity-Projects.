9fcb1182166984d9c52550bcc755b817a0d48ae8
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-02 21:12:30 -0700

    update

---
+++ Basic_C#_Programs/ClassesAndObjects/CallingMethod/Methods2/Methods2/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    class Operations
+    {
+        public int operations(int a, int b = 5)
+
+        {
+            int ans = a * b;
+            return ans;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassesAndObjects/CallingMethod/Methods2/Methods2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations math = new Operations();
+            Console.WriteLine("Please enter an integer.");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second integer (Optional).");
+            string Input = Console.ReadLine();
+
+            if (Input == "")
+            {
+                int ans = math.operations(a);
+                Console.WriteLine("Since you have decided to not input a second number we have given you a second number of 5.");
+                Console.Write(a + " * 5 = ");
+                Console.WriteLine(ans);
+            }
+            else
+            {
+                int b = Convert.ToInt32(Input);
+                int ans = math.operations(a, b);
+                Console.WriteLine("We will nwo multiply your two numbers to get the product.");
+                Console.Write(a + " * " + b + " = ");
+                Console.WriteLine(ans);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
