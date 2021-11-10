fc8189faa89da63819efe6807a8e96dcb16e5cc4
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-09 18:50:09 -0800

    update class

---
+++ Basic_C#_Programs/ClassesAndObjects/Class/Class/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Class
+{
+    class Math
+    {
+        public void UMath (int value1, int value2)
+        {
+            Console.WriteLine("\nThe first value which is {0} will be devided by 2 which equals {1}", value1, value1 / 2 );
+            Console.WriteLine("\nThen the second value is {0} will also be divided by 2 which equals {1}", value2, value2 / 2);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+    
+}
---
+++ Basic_C#_Programs/ClassesAndObjects/Class/Class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math operation = new Math();
+            Console.Write("We will be doing some math operations with some numbers.");
+            Console.WriteLine("");
+            Console.Write("Press enter to start the program.");
+            Console.ReadLine();
+
+            operation.UMath(20, 10);
+
+            Console.Write("Now we will do the same math with some numbers of your choice.");
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to begin ");
+            Console.ReadLine();
+
+            Console.Write("Please enter your first number: ");
+            int userVal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your second number: ");
+            int userVal2 = Convert.ToInt32(Console.ReadLine());
+            operation.UMath(userVal1, userVal2);
+
+
+            Console.Write("Press enter to end this program.");
+            Console.ReadLine();
+        }
+    }
+}
