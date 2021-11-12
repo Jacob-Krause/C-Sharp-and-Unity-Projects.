f11282cb79ead6d3173ad4e596d3e58b0b38b845
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-11 23:43:00 -0800

    update

--- Basic_C#_Programs/ClassesAndObjects/Abstract/Abstract/Employee.cs
+++ Basic_C#_Programs/ClassesAndObjects/Abstract/Abstract/Employee.cs
@@ -8,9 +8,6 @@ namespace Abstract
 {
     class Employee : Person
     {
-        public static int id = 1;
-        string FirstName = "Sample";
-        string LastName = "Student";
         public override void SayName()
         {
             base.SayName();
