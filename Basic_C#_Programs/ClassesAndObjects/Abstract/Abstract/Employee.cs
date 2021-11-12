a6ff3989bb90b0007bd3d594758e398b1032483e
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-11 23:37:36 -0800

    update

---
+++ Basic_C#_Programs/ClassesAndObjects/Abstract/Abstract/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract 
+{
+    class Employee : Person
+    {
+        public static int id = 1;
+        string FirstName = "Sample";
+        string LastName = "Student";
+        public override void SayName()
+        {
+            base.SayName();
+
+        }
+    }  
+}
