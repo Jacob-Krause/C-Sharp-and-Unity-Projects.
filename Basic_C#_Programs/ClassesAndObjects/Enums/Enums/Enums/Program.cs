ce45aaab94aa3a45404cd9a0ec4a146d2cbdee3f
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-14 21:29:57 -0800

    update

---
+++ Basic_C#_Programs/ClassesAndObjects/Enums/Enums/Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enterDay:
+            try
+            {
+                Console.Write("Please enter the current day of the week: ");
+                WeekDays Day = (WeekDays)Enum.Parse(typeof(WeekDays), Console.ReadLine(), true);
+                Console.WriteLine("The day of the week you chose is {0}", Day.ToString());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("\nPlease enter an actual day of the week.\n");
+                    goto enterDay;
+            }
+            Console.ReadLine();
+        }
+        public enum WeekDays
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
