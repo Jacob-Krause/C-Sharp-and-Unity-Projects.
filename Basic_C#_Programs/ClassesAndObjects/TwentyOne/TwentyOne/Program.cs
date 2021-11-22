21a74111e546cf11f70ff4ab0e75e8ec54663870
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-11-21 18:41:23 -0800

    Update

--- Basic_C#_Programs/ClassesAndObjects/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/ClassesAndObjects/TwentyOne/TwentyOne/Program.cs
@@ -10,20 +10,28 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. To start please tell us your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money have you brought to gamble with today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to play a game of 21?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
             }
-            Console.WriteLine("");
-            Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
-
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
-
     }
 }
 
